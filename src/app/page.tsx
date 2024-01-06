bdabf2c389bc1227bc04bd8ed5523cd0b09291e3
Gabriel Esmeraldo <gabriel-esmeraldo@hotmail.com>
2024-01-06 13:39:11 -0300

    feat: :sparkles: add header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>
-      <h1 className="bg-blue-500 text-red-500">Hello</h1>
-    </div>
-  );
+  return <div>home</div>;
 }
